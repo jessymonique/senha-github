d650dd80b481bd4ca245619b7b57434aee79790d
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-04-25 20:34:37 -0300

    exercicio senha

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+string senhaDigitada;
+string senhaEsperada = "monique";
+
+Console.Write("Digite sua senha..:");
+senhaDigitada = Console.ReadLine()!;
+
+if (senhaDigitada == senhaEsperada)
+{ 
+   // Sucesso no login 
+   Console.ForegroundColor = ConsoleColor.Green;
+   Console.WriteLine("Olá, usúario. Logado com sucesso.");
+} 
+
+else 
+{
+  // Fracasso no login 
+  Console.ForegroundColor = ConsoleColor.Red;
+ Console.WriteLine("Senha Inválida!");
+}
+
+Console.ResetColor();
+
+
